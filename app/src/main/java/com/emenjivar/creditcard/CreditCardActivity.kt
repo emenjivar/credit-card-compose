a89f6865d7cf5c7ac449e5c08a22c1ee2da36dac
carlos menjivar <carlos.itca@gmail.com>
2021-08-23 14:24:42 -0600

    :recycle: Move input components to its own file

--- app/src/main/java/com/emenjivar/creditcard/CreditCardActivity.kt
+++ app/src/main/java/com/emenjivar/creditcard/CreditCardActivity.kt
@@ -5,22 +5,13 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.*
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.emenjivar.creditcard.component.CreditCard
-import com.emenjivar.creditcard.utils.CardInputValidator
+import com.emenjivar.creditcard.model.CreditCardInputs
 import com.emenjivar.creditcard.viewmodel.CreditCardViewModel
 
 class CreditCardActivity : ComponentActivity() {
@@ -56,114 +47,6 @@ fun LayoutCreditCard(viewModel: CreditCardViewModel) {
         CreditCardInputs(viewModel = viewModel)
     }
 
-}
-
-@Composable
-fun CreditCardInputs(viewModel: CreditCardViewModel) {
-    val requesterHolderName = FocusRequester()
-    val requesterExpiration = FocusRequester()
-    val requesterCVC = FocusRequester()
-
-    Column(
-        modifier = Modifier.padding(16.dp)
-    ) {
-        TextField(
-            modifier = Modifier
-                .fillMaxWidth()
-                .onFocusChanged { state ->
-                    if(state.isFocused) viewModel.flipped = false
-                },
-            value = viewModel.number,
-            onValueChange = {
-                CardInputValidator.parseNumber(it)?.let { number ->
-                    viewModel.number = number
-                }
-            },
-            keyboardOptions = KeyboardOptions(
-                imeAction = ImeAction.Next,
-                keyboardType = KeyboardType.Number
-            ),
-            keyboardActions = KeyboardActions(
-                onNext = { requesterHolderName.requestFocus() }
-            ),
-            label = {
-                Text(text = "Number of card")
-            }
-        )
-
-        TextField(
-            modifier = Modifier
-                .fillMaxWidth()
-                .onFocusChanged { state ->
-                    if(state.isFocused) viewModel.flipped = false
-                }
-                .focusOrder(requesterHolderName),
-            value = viewModel.name,
-            onValueChange = {
-                CardInputValidator.parseHolderName(it)?.let { name ->
-                    viewModel.name = name
-                }
-            },
-            keyboardOptions = KeyboardOptions(
-                imeAction = ImeAction.Next
-            ),
-            keyboardActions = KeyboardActions(
-                onNext = { requesterExpiration.requestFocus() }
-            ),
-            label = {
-                Text("Name of card")
-            }
-        )
-
-        Row {
-            TextField(
-                value = viewModel.expiration,
-                modifier = Modifier
-                    .onFocusEvent { state ->
-                        if(state.isFocused) viewModel.flipped = false
-                    }
-                    .focusRequester(requesterExpiration),
-                onValueChange = {
-                    CardInputValidator.parseExpiration(it, viewModel.expiration)
-                        ?.let { expiration ->
-                            viewModel.expiration = expiration
-                        }
-                },
-                keyboardOptions = KeyboardOptions(
-                    imeAction = ImeAction.Next,
-                    keyboardType = KeyboardType.Number
-                ),
-                keyboardActions = KeyboardActions(
-                    onNext = { requesterCVC.requestFocus() }
-                ),
-                label = {
-                    Text("Expiration")
-                }
-            )
-            TextField(
-                modifier = Modifier
-                    .weight(0.5f)
-                    .onFocusEvent { state ->
-                        if(state.isFocused) viewModel.flipped = true
-                    }
-                    .focusRequester(requesterCVC),
-                value = viewModel.cvc,
-                onValueChange = {
-                    CardInputValidator.parseCVC(it)?.let { cvc ->
-                        viewModel.cvc = cvc
-                    }
-                },
-                keyboardOptions = KeyboardOptions(
-                    imeAction = ImeAction.Next,
-                    keyboardType = KeyboardType.Number
-                ),
-                label = {
-                    Text("Security code")
-                }
-            )
-        }
-
-    }
 }
 
 @Preview(showBackground = true)
---
+++ app/src/main/java/com/emenjivar/creditcard/model/CreditCardForm.kt
@@ -0,0 +1,132 @@
+package com.emenjivar.creditcard.model
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.focus.onFocusEvent
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import com.emenjivar.creditcard.utils.CardInputValidator
+import com.emenjivar.creditcard.viewmodel.CreditCardViewModel
+
+@Composable
+fun CreditCardInputs(viewModel: CreditCardViewModel) {
+    val focusHolderName = FocusRequester()
+    val focusExpiration = FocusRequester()
+    val focusCVC = FocusRequester()
+
+    Column(
+        modifier = Modifier.padding(16.dp)
+    ) {
+        FocusableTextField(
+            modifier = Modifier
+                .fillMaxWidth()
+                .onFocusChanged { state ->
+                    if (state.isFocused) viewModel.flipped
+                },
+            value = viewModel.number,
+            label = "Number of card",
+            onValueChange = {
+                CardInputValidator.parseNumber(it)?.let { number ->
+                    viewModel.number = number
+                }
+            },
+            keyboardType = KeyboardType.Number,
+            nextFocus = focusHolderName
+        )
+
+        FocusableTextField(
+            modifier = Modifier
+                .fillMaxWidth()
+                .onFocusChanged { state ->
+                    if (state.isFocused) viewModel.flipped = false
+                }
+                .focusRequester(focusHolderName),
+            value = viewModel.name,
+            label = "Name of card",
+            onValueChange = {
+                CardInputValidator.parseHolderName(it)?.let { name ->
+                    viewModel.name = name
+                }
+            },
+            nextFocus = focusExpiration
+        )
+
+        Row {
+            FocusableTextField(
+                modifier = Modifier
+                    .onFocusEvent { state ->
+                        if (state.isFocused) viewModel.flipped = false
+                    }
+                    .focusRequester(focusExpiration),
+                value = viewModel.expiration,
+                label = "Expiration",
+                onValueChange = {
+                    CardInputValidator.parseExpiration(it, viewModel.expiration)
+                        ?.let { expiration ->
+                            viewModel.expiration = expiration
+                        }
+                },
+                keyboardType = KeyboardType.Number,
+                nextFocus = focusCVC
+            )
+
+            FocusableTextField(
+                modifier = Modifier
+                    .weight(0.5f)
+                    .onFocusEvent { state ->
+                        if(state.isFocused) viewModel.flipped = true
+                    }
+                    .focusRequester(focusCVC),
+                value = viewModel.cvc,
+                label = "Security code",
+                onValueChange = {
+                    CardInputValidator.parseCVC(it)?.let { cvc ->
+                        viewModel.cvc = cvc
+                    }
+                },
+                keyboardType = KeyboardType.Number
+            )
+        }
+
+    }
+}
+
+@Composable
+private fun FocusableTextField(
+    modifier: Modifier,
+    value: String,
+    onValueChange: (String) -> Unit = {},
+    keyboardType: KeyboardType = KeyboardType.Text,
+    nextFocus: FocusRequester? = null,
+    label: String
+) {
+    val keyboardOptions = KeyboardOptions(
+        keyboardType = keyboardType,
+        imeAction = if(nextFocus != null) ImeAction.Next else ImeAction.Done
+    )
+
+    TextField(
+        value = value,
+        onValueChange = { onValueChange(it) },
+        modifier = modifier,
+        label = { Text(text = label) },
+        keyboardOptions = keyboardOptions,
+        keyboardActions = KeyboardActions(
+            onNext = {
+                nextFocus?.requestFocus()
+            }
+        )
+    )
+}
